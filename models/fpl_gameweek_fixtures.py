a5cdbd591a60c66e919fa7d3b49e22fb616e7064
khaid <khaidang28499@gmail.com>
2026-08-22 13:25:06 +0700

    fix logic display matches result and fixtures type

--- models/fpl_gameweek_fixtures.py
+++ models/fpl_gameweek_fixtures.py
@@ -68,8 +68,8 @@ class FplGameweekFixures(models.Model, FPLApiMixin):
         for rec in self:
             team_h_src = f'/fantasy_premier_league/static/src/img/teams_logo/{rec.team_h.code}.svg'
             team_a_src = f'/fantasy_premier_league/static/src/img/teams_logo/{rec.team_a.code}.svg'
-            if rec.team_h_score and rec.team_a_score and rec.started:
-                rec.match_result_display = f"<strong>{rec.team_h.name}</strong> <img src='{team_h_src}' style='width: 20px;border-radius: 10px;'/> {rec.team_h_score} - {rec.team_a_score} <img src='{team_a_src}' style='width: 20px;border-radius: 10px;'/> <strong>{rec.team_a.name}</strong>"
+            if rec.finished_provisional or rec.started:
+                rec.match_result_display = f"<strong>{rec.team_h.name}</strong> <img src='{team_h_src}' style='width: 20px;border-radius: 10px;'/> <strong>{rec.team_h_score} - {rec.team_a_score}</strong> <img src='{team_a_src}' style='width: 20px;border-radius: 10px;'/> <strong>{rec.team_a.name}</strong>"
             else:
                 local_kickoff = pytz.utc.localize(rec.kickoff_time).astimezone(user_tz)
                 rec.match_result_display = f"<strong>{rec.team_h.name}</strong> <img src='{team_h_src}' style='width: 20px;border-radius: 10px;'/> <span style='font-weight: bold; color: #34495E;'>{local_kickoff.strftime('%H:%M')}</span> <strong>{rec.team_a.name}</strong> <img src='{team_a_src}' style='width: 20px;border-radius: 10px;'/>"
